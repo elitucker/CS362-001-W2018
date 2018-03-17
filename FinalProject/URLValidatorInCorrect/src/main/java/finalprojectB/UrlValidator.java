ae273b07f334365c2493e71a45c3d4194b49b68e
elitucker <elitlaudi@gmail.com>
2018-03-17 13:39:19 -0700

    completed assignment

--- FinalProject/URLValidatorInCorrect/src/main/java/finalprojectB/UrlValidator.java
+++ FinalProject/URLValidatorInCorrect/src/main/java/finalprojectB/UrlValidator.java
@@ -164,7 +164,7 @@ public class UrlValidator implements Serializable {
      */
     private static final int PARSE_AUTHORITY_EXTRA = 4;
 
-    private static final String PATH_REGEX = "^(/[-\\w:@&?=+,.!*'%$_;\\(\\)]*)?$";
+    private static final String PATH_REGEX = "^(/[-\\w:@&?=+,.!*'%$_;\\(\\)]*)?$"; //deleted /~ from regex
     private static final Pattern PATH_PATTERN = Pattern.compile(PATH_REGEX);
 
     private static final String QUERY_REGEX = "^(\\S*)$";
@@ -279,7 +279,7 @@ public class UrlValidator implements Serializable {
             }
             allowedSchemes = new HashSet<String>(schemes.length);
             for(int i=0; i < schemes.length; i++) {
-                allowedSchemes.add(schemes[i].toUpperCase(Locale.ENGLISH));
+                allowedSchemes.add(schemes[i].toUpperCase(Locale.ENGLISH)); //should be .toLowerCase
 
             }
         }
--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -22,29 +22,110 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+      UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      System.out.println(validator.isValid("http://www.google.com")); //this properly returns that the url is valid
+      System.out.println(validator.isValid("www.google.co"));
+      System.out.println(validator.isValid("ww.google.com"));
+      System.out.println(validator.isValid("www./f.com"));
+      System.out.println(validator.isValid("google.com"));
+      System.out.println(validator.isValid("no://www.google.com")); //This assertion seems to reveal a bug (cannot convert to ascii)
    }
    
-   
-   public void testYourFirstPartition()
+
+   /**
+    * First partition
+    * In this partition, we were able to determine that the authority validator was being called in an improper way
+    * On line 318, the scheme is checked for http and if it is http, then the program checks to see if there is a port
+    * and invalidates the url if it finds one. This is incorrect. The scheme should be checked if it is a special case
+    * where the url is a local url. Basically, if the scheme is "file" it should not allow the port to be valid.
+    **/
+   public void testGoodUrls()
    {
-	 //You can use this function to implement your First Partition testing	   
+      final UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      final String[] schemes = {"http://", "ftp://", "h3t://"};
+      final String[] authorities = {"www.google.com", "0.0.0.0", "123.co"};
+      final String[] ports = {"", ":80", ":0"};
+      final String[] paths = {"", "/test", "/$23"};
 
+      for (int i = 0; i < schemes.length; i++)
+      {
+         for (int j = 0; j < authorities.length; j++)
+         {
+            for (int k = 0; k < ports.length; k++)
+            {
+               for (int m = 0; m < paths.length; m++)
+               {
+                  if (!validator.isValid(schemes[i] + authorities[j] + ports[k] + paths[m]))
+                  {
+                     System.out.println(validator.isValid(schemes[i] + authorities[j] + ports[k] + paths[m]));
+                     System.out.println(schemes[i] + authorities[j] + ports[k] + paths[m]); //whenever port is included in the good url, isValid claims it is bad
+                     System.out.println("-------");
+                  }
+               }
+            }
+         }
+      }
    }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
 
+
+   public void testBadUrls()
+   {
+      final UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      final String[] schemes = {"http://", "http:/", "3tp://"};
+      final String[] authorities = {"go.co", "1.2.3.4", "aaa."};
+      final String[] ports = {"", ":80", ":-1", ":65636"};
+      final String[] paths = {"", "/test1", "/..", "/test//$23"};
+
+      for (int i = 0; i < schemes.length; i++)
+      {
+         for (int j = 0; j < authorities.length; j++)
+         {
+            for (int k = 0; k < ports.length; k++)
+            {
+               for (int m = 0; m < paths.length; m++)
+               {
+                  if (validator.isValid(schemes[i] + authorities[j] + ports[k] + paths[m]))
+                  {
+                     System.out.println(m>0);
+                     System.out.println(validator.isValid(schemes[i] + authorities[j] + ports[k] + paths[m]));
+                     System.out.println(schemes[i] + authorities[j] + ports[k] + paths[m]); //whenever port is included in the good url, isValid claims it is bad
+                     System.out.println("-------");
+                  }
+               }
+            }
+         }
+      }
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
+
+
+   /**
+    * This method iterates through urls that have known validity, comparing the results from isValid() with the known validity of the url
+    */
    public void testIsValid()
    {
-	   //You can use this function for programming based testing
-
+      final UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      final String[] schemes = {"http://", "ftp://", "http:/", "3tp://"};
+      final Boolean[] schemesExpected = {true, true, false, false};
+      final String[] authorities = {"www.google.com", "go.co", "1.2.3.4", "aaa."};
+      final Boolean[] authoritiesExpected = {true, true, false, false};
+      final String[] ports = {"", ":80", ":-1", ":65636"};
+      final Boolean[] portsExpected = {true, true, false, false};
+      final String[] paths = {"", "/test1", "/..", "/test//$23"};
+      final Boolean[] pathsExpected = {true, true, false, false};
+      for (int i = 0; i < schemes.length; i++)
+      {
+         for (int j = 0; j < authorities.length; j++)
+         {
+            for (int k = 0; k < ports.length; k++)
+            {
+               for (int m = 0; m < paths.length; m++)
+               {
+                  final Boolean expected = schemesExpected[i] && authoritiesExpected[j] && portsExpected[k] && pathsExpected[m];
+                  final Boolean actual = validator.isValid(schemes[i] + authorities[j] + ports[k] + paths[m]);
+                  assertEquals(schemes[i] + authorities[j] + ports[k] + paths[m], expected, actual);
+               }
+            }
+         }
+      }
    }
-   
-
-
 }
